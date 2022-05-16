28d9d52ecdcecfa663938daf2c8a7126f29609e7
uuduru <102810279+uuduru@users.noreply.github.com>
2022-05-16 10:34:36 +0200

    Update nightwatch.conf.js

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -99,7 +99,7 @@ module.exports = {
       
       webdriver: {
         start_process: true,
-        server_path: '',
+        //server_path: '',
        server_path: 'node_modules/EdgeDriver/msedgedriver'
       }
     },
